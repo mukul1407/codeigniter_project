3158e59b125aad65059561e40c049514bc46b47e
Mukul Tiwari <150137424+mukul1407@users.noreply.github.com>
2024-11-18 19:41:51 +0530

    Update car_row.php

--- application/views/car_model/car_row.php
+++ application/views/car_model/car_row.php
@@ -3,7 +3,7 @@
     <td class="modelName"><?php echo $row['name'] ?></td>
     <td class="modelColor"><?php echo $row['color'] ?></td>
     <td class="modelTransmission"><?php echo $row['transmission'] ?></td>
-    <td class="modelPrice"><?php echo $row['price'] ?></td>
+    <td class="modelPrice">INR<?php echo $row['price'] ?></td>
     <td class="modelId"><?php echo $row['created_at'] ?></td>
     <td>
       <a href="javascript:void(0);" onclick="showEditForm(<?php echo $row['id'] ?>);" class="btn btn-primary">Edit</a>
